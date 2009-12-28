c380acde4bc006e7022fb5958e3ab60c20708097
winhamwr <winhamwr@gmail.com>
2009-12-28 17:35:32 -0500

    Added non-working logging mechanisms modeled off of celery

--- calabar/bin/calabard.py
+++ calabar/bin/calabard.py
@@ -7,9 +7,11 @@ from daemon import DaemonContext
 
 from ConfigParser import SafeConfigParser
 
+from calabar.log import setup_logger, redirect_stdouts_to_logger
 from calabar.tunnels import TunnelManager
 from calabar.tunnels.vpnc import VpncTunnel
 
+LOGFILE = '/var/log/calabar.log'
 VPNC_CONF = '/etc/calabar/default.conf'
 CALABAR_CONF = '/etc/calabar/calabar.conf'
 
@@ -20,19 +22,32 @@ OPTION_LIST = (
     optparse.make_option('-d', '--daemon', action="store_true", dest="daemon",
                          default=False,
                          help="Run in the background as a daemon"),
+    optparse.make_option('-f', '--logfile', default=LOGFILE,
+                         action="store", dest="logfile",
+                         help="Path to log file."),
 )
 
 
-def run_tunnels(configfile='/etc/calabar/calabar.conf', daemon=False):
+def run_tunnels(configfile='/etc/calabar/calabar.conf', daemon=False,
+                logfile=LOGFILE):
     """Run the configured VPN/SSH tunnels and keep them running"""
     config = SafeConfigParser()
     config.read(configfile)
 
     tm = TunnelManager()
     if daemon:
+        # Since without stderr any errors will be silently suppressed,
+        # we need to know that we have access to the logfile
+        if logfile:
+            open(logfile, "a").close()
         with DaemonContext():
+            logger = setup_logger(logfile=logfile)
+            redirect_stdouts_to_logger(logger)
+
             _run_tunnels(tm, config)
     else:
+        logger = setup_logger(logfile=logfile)
+        redirect_stdouts_to_logger(logger)
         _run_tunnels(tm, config)
 
 def _run_tunnels(tm, config):
